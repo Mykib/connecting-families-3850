60284793522c0d7d11af9ae111b661bc53a2e3f1
Mykib <m.brims93@gmail.com>
2021-04-23 03:00:31 +1000

    Formik + Yup validation for signup dialog

--- src/components/SignUpDialog.js
+++ src/components/SignUpDialog.js
@@ -9,6 +9,8 @@ import {
   DialogTitle,
 } from "@material-ui/core";
 import IconButton from "./IconButton";
+import { useFormik } from "formik";
+import * as yup from "yup";
 import firebase from "firebase";
 import ButtonGoogleSignIn from "./ButtonGoogleSignIn";
 import ButtonFacebookSignIn from "./ButtonFacebookSignIn";
@@ -18,6 +20,36 @@ function SignUpDialog(props) {
   const user = userProvider.useUserContext();
   const setUser = userProvider.useUserContextUpdate();
   const open = true;
+
+  const validationSchema = yup.object({
+    firstName: yup.string().required("Required"),
+    surname: yup.string().required("Required"),
+    email: yup
+      .string()
+      .email("Please enter valid email")
+      .required("Required"),
+    password: yup
+      .string()
+      .min(8, ({ min }) => `Must be at least ${min} characters`)
+      .required("Required"),
+    confirmation: yup
+      .string()
+      .oneOf([yup.ref("password"), null], "Passwords must match"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      surname: "",
+      email: "",
+      password: "",
+      confirmation: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
   const handleClickClose = () => {
     props.setOpen(false);
@@ -31,63 +63,87 @@ function SignUpDialog(props) {
         aria-labelledby="form-dialog-title"
         // className="dialog-card"
       >
-        <IconButton onClick={handleClickClose} />
-        <DialogTitle>Sign Up</DialogTitle>
-        <DialogContent className="dialog-content">
-          <div className="single-row">
+        <form onSubmit={formik.handleSubmit}>
+          <IconButton onClick={handleClickClose} />
+          <DialogTitle>Sign Up</DialogTitle>
+          <DialogContent className="dialog-content">
+            <div className="single-row">
+              <TextField
+                autoFocus
+                id="firstName"
+                name="firstName"
+                label="First Name"
+                type="name"
+                autoComplete="off"
+                value={formik.values.firstName}
+                onChange={formik.handleChange}
+                error={formik.touched.firstName && Boolean(formik.errors.firstName)}
+                helperText={formik.touched.firstName && formik.errors.firstName}
+              />
+              <TextField
+                id="surname"
+                name="surname"
+                label="Surname"
+                type="name"
+                autoComplete="off"
+                value={formik.values.surname}
+                onChange={formik.handleChange}
+                error={formik.touched.surname && Boolean(formik.errors.surname)}
+                helperText={formik.touched.surname && formik.errors.surname}
+              />
+            </div>
             <TextField
-              autoFocus
-              id="name"
-              label="First Name"
-              type="name"
+              id="email"
+              name="email"
+              label="Email Address"
+              type="email"
               autoComplete="off"
+              fullWidth
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
             />
             <TextField
-              id="surname"
-              label="Surname"
-              type="name"
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
               autoComplete="off"
+              fullWidth
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
             />
-          </div>
-          <TextField
-            autoFocus
-            id="name"
-            label="Email Address"
-            type="email"
-            autoComplete="off"
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            id="password"
-            label="Password"
-            type="password"
-            autoComplete="off"
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            id="confirmPassword"
-            label="Confirm Password"
-            type="password"
-            autoComplete="off"
-            fullWidth
-          />
-          <Button onClick={props.goBack} color="primary" size="small">
-            have an account? Login Here
-          </Button>
-          <ButtonGoogleSignIn />
-          <ButtonFacebookSignIn />
-        </DialogContent>
-        <DialogActions>
-          <Button
-            onClick={handleClickClose}
-            variant="contained"
-            color="primary"
-          >
-            Sign In
-          </Button>
-        </DialogActions>
+            <TextField
+              id="confirmation"
+              name="confirmation"
+              label="Confirm Password"
+              type="password"
+              autoComplete="off"
+              fullWidth
+              value={formik.values.confirmation}
+              onChange={formik.handleChange}
+              error={formik.touched.confirmation && Boolean(formik.errors.confirmation)}
+              helperText={formik.touched.confirmation && formik.errors.confirmation}
+            />
+            <Button onClick={props.goBack} color="primary" size="small">
+              have an account? Login Here
+            </Button>
+            <ButtonGoogleSignIn />
+            <ButtonFacebookSignIn />
+          </DialogContent>
+          <DialogActions>
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+            >
+              Sign Up
+            </Button>
+          </DialogActions>
+        </form>
       </Dialog>
     </div>
   );
