0383df2aee11c6aa76db880498dd4cff002f8583
Mykib <m.brims93@gmail.com>
2021-04-25 03:11:45 +1000

    Responsive navbar - small viewport implemented

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,35 +1,60 @@
 import "./NavBar.scss";
 
+import React, { useEffect, useState } from "react";
+
 import { Button } from "@material-ui/core";
-import React from "react";
+import { Squash as Hamburger } from "hamburger-react";
 import SignInButton from "./SignInButton";
 import aaLogo from "../assets/activities-australia-logo-vector.svg";
 import leafIcon from "../assets/connecting-families-wired-logo.svg";
 
 function NavBar(props) {
-  const navItems = ["programs", "about", "ndis", "contact"];
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [navItems, setNavItems] = useState([
+    "programs",
+    "about",
+    "ndis",
+    "contact",
+  ]);
+
+  useEffect(() => {
+    menuOpen
+      ? setNavItems(["home", "programs", "about", "ndis", "contact"])
+      : setNavItems(["programs", "about", "ndis", "contact"]);
+  }, [menuOpen]);
 
   return (
     <div className="nav-bar">
-      <div className="ca-home">
+      <div className="nav-mobile">
+        <div className="hamburger">
+          <Hamburger toggled={menuOpen} toggle={setMenuOpen} size={20} />
+        </div>
         <img src={leafIcon} alt="leaf-logo" className="leaf-logo" />
         <div className="ca-text">
           <h1>Connecting Families</h1>Bringing families together
         </div>
       </div>
-      <div className="nav-spacer"></div>
-      <div className="nav-items">
-        {navItems.map((item) => (
-          <Button variant="text" className="nav-button">
-            {item}
-          </Button>
-        ))}
-      </div>
-      <div className="aa-link">
-        <img src={aaLogo} alt="aa-logo" />
-      </div>
-      <div className="account-button">
-        <SignInButton />
+      <div className={`nav-content ${menuOpen ? "not-hidden" : "hidden"}`}>
+        <div className="ca-home">
+          <img src={leafIcon} alt="leaf-logo" className="leaf-logo" />
+          <div className="ca-text">
+            <h1>Connecting Families</h1>Bringing families together
+          </div>
+        </div>
+        <div className="nav-spacer"></div>
+        <div className="nav-items">
+          {navItems.map((item) => (
+            <Button variant="text" className="nav-button">
+              {item}
+            </Button>
+          ))}
+        </div>
+        <div className="aa-link">
+          <img src={aaLogo} alt="aa-logo" />
+        </div>
+        <div className="account-button">
+          <SignInButton />
+        </div>
       </div>
     </div>
   );
